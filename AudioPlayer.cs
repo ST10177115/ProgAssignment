a5fd844d17b932b0b853e27f7d528d467c4162b5
ST10177115 <167039827+ST10177115@users.noreply.github.com>
2026-04-13 22:55:54 +0200

    Add files via upload

---
+++ AudioPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Media;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgAssignment
+{
+    public class AudioPlayer
+    {
+        public static void PlayGreeting()
+        {
+            SoundPlayer player = new SoundPlayer("greeting.wav.wav");
+            player.PlaySync();
+        }
+    }
+}
+
