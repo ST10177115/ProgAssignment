900e8c53272197310c0444d1c0c0cdf8d333fa9e
ST10177115 <167039827+ST10177115@users.noreply.github.com>
2026-04-13 22:55:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ProgAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UIHelper.ShowHeader();
+
+            AudioPlayer.PlayGreeting();
+
+            Chatbot bot = new Chatbot();
+            bot.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
