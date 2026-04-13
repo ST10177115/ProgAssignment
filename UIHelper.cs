5ee58a6de4c170c8aa06e234fbf466830c428a2c
ST10177115 <167039827+ST10177115@users.noreply.github.com>
2026-04-13 22:56:35 +0200

    Add files via upload

---
+++ UIHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Media;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgAssignment
+{
+    public static class UIHelper
+    {
+        public static void ShowHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("========================================");
+            Console.WriteLine("   CYBERSECURITY AWARENESS BOT");
+            Console.WriteLine("========================================");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(@"
+   [🔒]   Stay Safe Online   [🔒]
+
+      ___
+     /       \
+    |  SAFE  |
+    |  NET   |
+     \___/
+        | |
+        | |
+    ");
+
+            Console.ResetColor();
+        }
+
+        public static void TypingEffect(string message)
+        {
+            foreach (char c in message)
+            {
+                Console.Write(c);
+                System.Threading.Thread.Sleep(20);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
